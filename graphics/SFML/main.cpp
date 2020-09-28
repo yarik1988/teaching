b90be2897ae8b89355f6ada4860e23fc8e92aae2
yarik1988 <yaroslav.linder@gmail.com>
2020-09-28 12:19:16 +0300

    update

--- graphics/SFML/main.cpp
+++ graphics/SFML/main.cpp
@@ -3,8 +3,18 @@
 
 int main() {
     // create the window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-
+    sf::RenderWindow window(sf::VideoMode(800, 800), "My window");
+    float rad=100;
+    float angle=0.01;
+    sf::ConvexShape square;
+    square.setPointCount(4);
+    square.setPoint(0, sf::Vector2f(-rad, -rad));
+    square.setPoint(1, sf::Vector2f(-rad, rad));
+    square.setPoint(2, sf::Vector2f(rad, rad));
+    square.setPoint(3, sf::Vector2f(rad, -rad));
+    square.setFillColor(sf::Color(0,100,100));
+    sf::View view(sf::FloatRect(-2*rad,-2*rad, 4*rad, 4*rad));
+    window.setView(view);
     // run the program as long as the window is open
     while (window.isOpen())
     {
@@ -22,6 +32,9 @@ int main() {
 
         // draw everything here...
         // window.draw(...);
+        square.rotate(angle);
+        window.draw(square);
+
 
         // end the current frame
         window.display();
