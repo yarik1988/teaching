f2647ce5b28ccecd2721c9e647d44cb6e3c87a51
yarik1988 <yaroslav.linder@gmail.com>
2020-09-29 10:23:59 +0300

    update

--- graphics/SFML/main.cpp
+++ graphics/SFML/main.cpp
@@ -6,13 +6,24 @@ int main() {
     float rad=100;
     const float init_angle_speed=0.01;
     float angle=init_angle_speed;
-    sf::ConvexShape square;
-    square.setPointCount(4);
-    square.setPoint(0, sf::Vector2f(-rad, -rad));
-    square.setPoint(1, sf::Vector2f(-rad, rad));
-    square.setPoint(2, sf::Vector2f(rad, rad));
-    square.setPoint(3, sf::Vector2f(rad, -rad));
-    square.setFillColor(sf::Color(0,100,100));
+    sf::VertexArray square(sf::Quads, 4);
+    square[0].position=sf::Vector2f(-rad/2, -rad/2);
+    square[1].position=sf::Vector2f(-rad/2, rad/2);
+    square[2].position=sf::Vector2f(rad/2, rad/2);
+    square[3].position=sf::Vector2f(rad/2, -rad/2);
+    square[0].color=sf::Color::Red;
+    square[1].color=sf::Color::Green;
+    square[2].color=sf::Color::Magenta;
+    square[3].color=sf::Color::Blue;
+
+    sf::ConvexShape square_rot;
+    square_rot.setPointCount(4);
+    square_rot.setPoint(0, sf::Vector2f(-rad, -rad));
+    square_rot.setPoint(1, sf::Vector2f(-rad, rad));
+    square_rot.setPoint(2, sf::Vector2f(rad, rad));
+    square_rot.setPoint(3, sf::Vector2f(rad, -rad));
+    square_rot.setFillColor(sf::Color(0,100,100));
+
     sf::View view(sf::FloatRect(-2*rad,-2*rad, 4*rad, 4*rad));
     window.setView(view);
     // run the program as long as the window is open
@@ -42,9 +53,9 @@ int main() {
 
         // draw everything here...
         // window.draw(...);
-        square.rotate(angle);
+        square_rot.rotate(angle);
+        window.draw(square_rot);
         window.draw(square);
-
 
         // end the current frame
         window.display();
